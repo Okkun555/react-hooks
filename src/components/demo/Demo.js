8eea8de8070be8038bbbd5e6a34cd74b6183805e
Okkun555 <y.okuno.btm@gmail.com>
2022-12-08 21:49:04 +0900

    3-13

--- src/components/demo/Demo.js
+++ src/components/demo/Demo.js
@@ -1,23 +1,63 @@
-import React from "react";
+import React, { useState, useReducer } from "react";
+import reducer from "../../reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Demo = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  // NOTE: useReducer(reducer, 初期状態, 初期化時に実施したい処理)
+  const [state, dispatch] = useReducer(reducer, []);
+
+  const addEvent = (e) => {
+    e.preventDefault();
+
+    dispatch({
+      type: "CREATE_EVENT",
+      title: title,
+      body: body,
+    });
+
+    setTitle("");
+    setBody("");
+  };
+
+  const deleteAllEvent = (e) => {
+    e.preventDefault();
+    dispatch({ type: "DELETE_ALL_EVENTS" });
+  };
+
+  console.log(state);
+
   return (
     <div className="container">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" type="text" />
+          <input
+            className="form-control"
+            id="formEventTitle"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="formEventBody">内容</label>
-          <textarea className="form-control" id="formEventBody" type="text" />
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            type="text"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
-        <button className="btn btn-danger ml-2">
+        <button onClick={addEvent} className="btn btn-primary">
+          イベントを作成する
+        </button>
+        <button onClick={deleteAllEvent} className="btn btn-danger ml-2">
           全てのイベントを削除する
         </button>
       </form>
